aeeb8e57edb1049f2b5dcff72bcec58646332c32
SerhiiPochtovyk <sergey.po4tovik@gmail.com>
2023-09-21 09:32:39 +0300

    create  js-lightbox-js; add lightbox files;

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,34 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const galleryRef = document.querySelector(".gallery");
+
+function addGalleryMarkup(images) {
+  const markup = images.map((image) => createGalleryMarkup(image)).join("");
+
+  return (galleryRef.innerHTML = markup);
+}
+
+function createGalleryMarkup(image) {
+  const { original, preview, description } = image;
+  return `<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>`;
+}
+
+addGalleryMarkup(galleryItems);
+
+galleryRef.addEventListener("click", onImageClick);
+
+function onImageClick(event) {
+  event.preventDefault();
+
+  if (event.target.nodeName !== "IMG") {
+    return;
+  }
+}
+
+const gallery = new SimpleLightbox(".gallery a", {
+  captionDelay: 250,
+  captionsData: "alt",
+});
